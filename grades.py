10f8f4bf0cc20a569006e17e5220b6cb83786bf9
nico1095 <agiso@students.kennesaw.edu>
2025-09-18 07:55:28 -0400

    Update grades.py

--- grades.py
+++ grades.py
@@ -1,1 +1,7 @@
-
+# Program Name: grades.py 
+# Course: IT3883/Section W02
+# Student Name: Anthony Giso
+# Assignment Number: 2
+# Due Date: 09/19/ 2025
+# Purpose: python program that calculates the final averages for a group of students and print the results in descending order by grade. 
+# Used Python module and chapters.
